587587861b51a8bb1525822c7c9dcc456360026d
denyskarakycha <denys.karakycha@gmail.com>
2023-10-27 17:34:39 +0300

    Added basic crud opr

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { ITask } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,6 +19,24 @@ export class TasksController {
   @Get()
   getAllTasks(): ITask[] {
     return this.taskService.getAllTasks();
+  }
+
+  @Get('/single/:id')
+  getTask(@Param('id') id: string): ITask {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Delete('/single/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    return this.taskService.deleteTaskById(id);
+  }
+
+  @Patch('/single/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): ITask {
+    return this.taskService.updateTaskStatus(id, status);
   }
 
   @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,20 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): ITask {
+    return this.tasks.find((task: ITask) => id === task.id);
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task: ITask) => id !== task.id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): ITask {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
   createTask(createTaskDto: CreateTaskDto): ITask {
     const { title, description } = createTaskDto;
 
